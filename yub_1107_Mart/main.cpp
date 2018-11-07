493ae880ac794ec3e7f7a150a39c47b41ec8d05d
EunbeeYun <8642166@naver.com>
2018-11-07 15:46:51 +0900

    Add : Calculate Mode

--- yub_1107_Mart/main.cpp
+++ yub_1107_Mart/main.cpp
@@ -1,8 +1,14 @@
 #include "myFunc.h"
 
-
 unsigned int time_count = 0, sec = 0, min = 0, hour = 0;
 int mode = NORMAL_MODE;
+int number[10] = {KEY_0, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9};
+unsigned int product_price = 0;
+unsigned int total_money = 0;
+unsigned int input_money = 0;
+char buf1[16];
+int flag = 0;
+int change = 0;
 
 ISR(TIMER0_OVF_vect)
 {
@@ -20,25 +26,122 @@ ISR(TIMER0_OVF_vect)
 int main(void)
 {
 	init();
+	char buff[16];
 	unsigned int input_key = 0;
     while (1) 
     {
 		input_key = getkey();
+		if (input_key == KEY_LOBY) 
+		{
+			lcd_clear();
+			mode = NORMAL_MODE;		
+		}
 		if (mode == NORMAL_MODE)
 		{
 			Display_Clock(hour, min, sec);
-			if (input_key == KEY_Munu) mode = MENU_MODE;
+			if (input_key == KEY_Menu) 
+			{
+				lcd_clear();
+				mode = MENU_MODE;
+			}
 		}
-		if (mode == MENU_MODE)
+		else if (mode == MENU_MODE)
 		{
 			lcd_putsf(0,0,(unsigned char *)"1: Calculation  ");
 			lcd_putsf(0,1,(unsigned char *)"2: Total Sales  ");
 			
-			if (input_key == KEY_1) mode = CALC_MODE;
-			if (input_key == KEY_2) mode = TOTAL_MODE;
-			input_key = 0; 
+			if (input_key == KEY_1) 
+			{
+				lcd_clear();
+				mode = CALC_MODE;
+			}
+			if (input_key == KEY_2) 
+			{
+				lcd_clear();
+				mode = TOTAL_MODE;
+			}
 			_delay_ms(50);
+		}	
+		else if (mode == CALC_MODE)
+		{
+			lcd_putsf(0,0,(unsigned char *)"Calculate Mode  ");
+			
+			if (input_key == 0) flag = 0;
+			for (int i = 0; i < 10; i++)
+			{
+				if ((input_key == number[i]) && (flag == 0))
+				{
+					flag = 1;
+					product_price = product_price * 10 + i;					
+					sprintf(buf1,"%d",product_price);
+					lcd_putsf(0,1,(unsigned char *)buf1);
+					//break;
+				}
+			}
+			if ((input_key == KEY_PLUS) && (flag == 0))
+			{
+				flag = 1;			
+				
+				if (product_price > 20000)
+				{
+					sprintf(buf1,"     ERROR!      ");
+					for (int j = 0; j < 3; j++)
+					{
+						lcd_putsf(0,1,(unsigned char *)buf1);
+						_delay_ms(100);
+						lcd_putsf(0,1,(unsigned char *)"                ");
+						_delay_ms(100);
+					}
+					mode = NORMAL_MODE;
+					total_money = 0;
+				}
+				else
+				{
+					lcd_putsf(0,1,(unsigned char *)"                ");
+					total_money += product_price;
+					product_price = 0;	
+				}				
+			}
+			if ((input_key == KEY_Enter) && (flag == 0))
+			{
+				flag = 1;
+				if (product_price > 20000)
+				{
+					sprintf(buf1,"     ERROR!     ");
+					for (int j = 0; j < 3; j++)
+					{
+						lcd_putsf(0,1,(unsigned char *)buf1);
+						_delay_ms(100);
+						lcd_putsf(0,1,(unsigned char *)"                ");
+						_delay_ms(100);
+					}
+					mode = NORMAL_MODE;
+					total_money = 0;
+				}
+				else
+				{
+					total_money += product_price;
+					sprintf(buf1,"=%11d won",total_money);
+					lcd_putsf(0,1,(unsigned char *)buf1);
+					change = 1;
+				}
+			}
+			if ((input_key == KEY_MONEY) && (change == 1))
+			{
+				lcd_clear();
+				mode = CHANGES_MODE;
+			}
+		}
+		else if (mode == CHANGES_MODE)
+		{
+			lcd_putsf(0,0,(unsigned char *)"Changes Mode    ");
+			change = 0;
+		}
+		else if (mode == TOTAL_MODE)
+		{
+			lcd_putsf(0,0,(unsigned char *)"Total Sales     ");
 		}		
+
     }
 }
 
--- yub_1107_Mart/myFunc.h
+++ yub_1107_Mart/myFunc.h
@@ -8,10 +8,11 @@
 #include <stdio.h>
 #include "lcd.h"
 
-#define NORMAL_MODE 0
-#define MENU_MODE   1
-#define CALC_MODE   2
-#define TOTAL_MODE  3
+#define NORMAL_MODE  0
+#define MENU_MODE    1
+#define CALC_MODE    2
+#define CHANGES_MODE 3
+#define TOTAL_MODE  4
 
 #define KEY_1 238
 #define KEY_2 222
@@ -25,7 +26,7 @@
 #define KEY_0 231
 #define KEY_PLUS  126
 #define KEY_Enter 125
-#define KEY_Munu  123
+#define KEY_Menu  123
 #define KEY_MONEY 183
 #define KEY_LOBY  119
 
@@ -38,6 +39,7 @@ void init()
 				  
 	TIMSK = 0x01;
 	TCCR0 = 0x07;
+	_delay_ms(2);
 	
 	lcd_init();
 	lcd_clear();
@@ -47,10 +49,10 @@ void init()
 		
 		lcd_putsf(0,0,(unsigned char*)"     MARKET     ");
 		lcd_putsf(0,1,(unsigned char*)"     SYSTEM     ");
-		PORTA = 0b00000000;
+		PORTA = 0x00;
 		_delay_ms(100);
 		lcd_clear();
-		PORTA = 0b01110000;
+		PORTA = 0xff;
 		_delay_ms(100);
 	}
 	sei(); // Global Interrupt Enable
@@ -69,7 +71,7 @@ unsigned int getkey()
 	for(int i = 0; i < 4; i++)
 	{
 		PORTD = ~(0x00 | 1 << i); 
-		_delay_ms(5);
+		_delay_ms(10);
 		key = (~PIND & 0xf0); // key가 눌리지 않았을 때 0값을 출력하도록 함
 		
 		if (key) return PIND; // key가 눌렸다면 0이 아니므로 고유값 PIND를 반환
